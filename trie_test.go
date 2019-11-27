8273e9cbe6c3883a067694639c7f4a9e4204bf16
张永胜 <yongsheng7@staff.sina.com.cn>
2019-11-27 17:55:58 +0800

    ref: 添加单元测试

--- trie_test.go
+++ trie_test.go
@@ -4,6 +4,25 @@ import (
 	"testing"
 )
 
+func TestTrie_Find(t *testing.T) {
+	trie := NewTrie()
+	keyList := []string{"ABCD", "ABC", "AB", "A", "B", "C", "BCD"}
+
+	for _, key := range keyList {
+		trie.Insert([]byte(key), key)
+	}
+
+	for _, key := range keyList {
+		ret, value := trie.Find([]byte(key))
+		if ret == false {
+			t.Error("find or insert error")
+		}
+		if value != key {
+			t.Error("key value is error")
+		}
+	}
+}
+
 func TestTrie_Remove(t *testing.T) {
 	trie := NewTrie()
 
@@ -18,14 +37,14 @@ func TestTrie_Remove(t *testing.T) {
 		if ok {
 			t.Error("remove failed")
 		}
-		if trie.NumberKey != uint(len(keyList)) {
+		if trie.NumberKey != int32(len(keyList)) {
 			t.Error("remove failed")
 		}
 	}
 
 	for idx, key := range keyList {
 		trie.Remove([]byte(key))
-		if trie.NumberKey != uint(len(keyList)-idx-1) {
+		if trie.NumberKey != int32(len(keyList)-idx-1) {
 			t.Error("remove test failed")
 		}
 	}
